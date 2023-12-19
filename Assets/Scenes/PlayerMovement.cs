92fdb3cf425f0a037fb2697bb778765985ec76b2
Josh Lawlor <joshlawlor1@gmail.com>
2023-12-18 21:58:42 -0800

    Player movement controlled by mouse input

--- Assets/Scenes/PlayerMovement.cs
+++ Assets/Scenes/PlayerMovement.cs
@@ -4,16 +4,15 @@ using System.Numerics;
 using Unity.VisualScripting;
 using UnityEngine;
 using Vector2 = UnityEngine.Vector2;
-
+using Vector3 = UnityEngine.Vector3;
 public class PlayerMovement : MonoBehaviour
 {
-
     public float moveSpeed;
     public Rigidbody2D rb;
     public Animator animator;
 
     private Vector2 moveDirection;
-    // Update is called once per frame
+
     void Update()
     {
         ProcessInputs();
@@ -22,18 +21,27 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
-        //Physics Calculations
     }
 
     void ProcessInputs()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Horizontal", moveX);
-        animator.SetFloat("Vertical", moveY);
-        animator.SetFloat("Speed", moveSpeed);
-        moveDirection = new Vector2(moveX, moveY).normalized;
 
+        // Calculate mouse direction
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 directionToMouse = (mousePosition - transform.position).normalized;
+
+        // Combine keyboard and mouse movement
+        moveDirection = new Vector2(moveX, moveY) + directionToMouse;
+
+        // Normalize to ensure consistent speed
+        moveDirection = moveDirection.normalized;
+
+        // Update Animator parameters
+        animator.SetFloat("Horizontal", moveDirection.x);
+        animator.SetFloat("Vertical", moveDirection.y);
+        animator.SetFloat("Speed", moveDirection.magnitude);
     }
 
     void Move()
