2fa69bf6b4a66ffcda48e6a0290ebdf0741d7e48
Josh Lawlor <joshlawlor1@gmail.com>
2024-01-02 10:06:58 -0800

    Item and Player Data Class scripts setup

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class PlayerData : ScriptableObject
+{
+   public string playerName;
+   public int currentScore;
+}
