026f2d75b416f7f8516f95ea44771c5dbdd19151
Josh Lawlor <joshlawlor1@gmail.com>
2024-01-11 19:27:35 -0800

    Added AStarPathfinding. NPC wanders on walkable grid

---
+++ Assets/NPCAnimation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAnimation : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Start()
+    {
+        // Get the Animator component
+        animator = GetComponent<Animator>();
+        
+        if (animator == null)
+        {
+            Debug.LogError("Animator component not found on NPC.");
+        }
+    }
+
+    public void UpdateAnimation(Vector3 targetPosition, float moveSpeed)
+    {
+        // Calculate move direction
+        Vector3 moveDirection = (targetPosition - transform.position).normalized;
+
+        // Set animator parameters
+        if (animator != null)
+        {
+            animator.SetFloat("Horizontal", moveDirection.x);
+            animator.SetFloat("Vertical", moveDirection.y);
+            animator.SetFloat("Speed", moveSpeed);
+        }
+    }
+}
