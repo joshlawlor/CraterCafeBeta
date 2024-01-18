5c8e2ca900a5ac0b322ea4a9fa83eb7e09fc49bd
Josh Lawlor <joshlawlor1@gmail.com>
2024-01-18 15:14:04 -0800

    BarGlassController added. Inventory UI + Script added to Bar scene

---
+++ Assets/Scripts/BarObjects/BarGlassController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarGlassController : MonoBehaviour
+{
+    public GameObject InteractPopUp;
+    private bool playerInRange = false;
+    public ItemData itemData;
+
+    private void SetPopUpActive(GameObject popUp, bool isActive)
+    {
+        if (popUp != null)
+        {
+            popUp.SetActive(isActive);
+        }
+    }
+
+    private void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E) && Inventory.Instance.inventory.Count < 1)
+        {
+            Inventory inventory = FindObjectOfType<Inventory>();
+
+            if (inventory != null && itemData != null)
+            {
+                Inventory.Instance.Add(itemData);
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            SetPopUpActive(InteractPopUp, true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            SetPopUpActive(InteractPopUp, false);
+
+        }
+    }
+
+}
