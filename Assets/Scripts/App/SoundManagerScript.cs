51fa57aeec2e40c587a8e98408dc440ce3f61a45
Josh Lawlor <joshlawlor1@gmail.com>
2024-01-29 07:21:23 -0800

    SoundManagerScript functional. Will play selected audi clip when scene starts

--- Assets/Scripts/App/SoundManagerScript.cs
+++ Assets/Scripts/App/SoundManagerScript.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class SoundManagerScript : MonoBehaviour
 {
-    public static AudioClip barMusic;
+    public AudioClip barMusic;
     static AudioSource audioSource;
 
+    // Start is called before the first frame update
+    void Start()
+    {
 
+        // Create an AudioSource component if it doesn't exist
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        // Play the audio clip
+        PlayBarMusic();
+    }
+
+    // Function to play the "barMusic" audio clip
+    public void PlayBarMusic()
+    {
+        if (audioSource != null && barMusic != null)
+        {
+            audioSource.clip = barMusic;
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogError("AudioSource or barMusic clip is not set in the SoundManagerScript.");
+        }
+    }
 }
