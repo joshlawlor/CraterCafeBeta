9a1c8886ad6417c47131e78b8688a4851927b4c9
Josh Lawlor <joshlawlor1@gmail.com>
2024-01-30 10:39:39 -0800

    ClockFace now rotates based on current DateTime

--- Assets/Scripts/App/ClockManagerScript.cs
+++ Assets/Scripts/App/ClockManagerScript.cs
@@ -7,21 +7,40 @@ using DPUtils.Systems.DateTime;
 
 public class ClockManagerScript : MonoBehaviour
 {
+    public RectTransform ClockFace;
+
+    private float startingRotation;
     public TextMeshProUGUI Date, Time, Season, Week;
 
-    private void OnEnable(){
+    public void Awake()
+    {
+
+        startingRotation = ClockFace.localEulerAngles.z;
+    }
+
+    private void OnEnable()
+    {
         TimeManagerScript.OnDateTimeChanged += UpdateDateTime;
     }
 
-    private void OnDisable(){
+    private void OnDisable()
+    {
         TimeManagerScript.OnDateTimeChanged -= UpdateDateTime;
     }
 
-    private void UpdateDateTime(DateTime dateTime) {
+    private void UpdateDateTime(DateTime dateTime)
+    {
         Date.text = dateTime.DateToString();
         Time.text = dateTime.TimeToString();
         Season.text = dateTime.Season.ToString();
         Week.text = $"WEEK: {dateTime.CurrentWeek.ToString()}";
-        
+
+        float t = (float)dateTime.Hour / 24f;
+
+        float newRotation = Mathf.Lerp(0, 360, t);
+        ClockFace.localEulerAngles = new Vector3(0, 0, newRotation + startingRotation);
+
+
+
     }
 }
